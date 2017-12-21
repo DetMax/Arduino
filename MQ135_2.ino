20705355c9eaa5f634580c23dbaa6ca135a37725
DetMax <31571120+DetMax@users.noreply.github.com>
2017-12-21 23:46:58 +0530

    Add files via upload

---
+++ MQ135_2.ino
@@ -0,0 +1,20 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+ 
+void loop() 
+{
+  float sValue;
+  float sVolt; 
+  
+ 
+  sValue = analogRead(A0);
+  sVolt = sValue/1024*5.0;
+ 
+  Serial.print("sensor voltage = ");
+  Serial.print(sVolt);
+  Serial.print("V ");
+  Serial.println(sValue);
+  delay(1000);
+}
