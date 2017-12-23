e254e85389344f727f82bd8ff9284c7e85be1d49
DetMax <31571120+DetMax@users.noreply.github.com>
2017-12-23 20:46:57 +0530

    Add files via upload

---
+++ DHT11_1.ino
@@ -0,0 +1,26 @@
+#include<dht.h>
+dht DHT;
+
+void setup() {
+
+Serial.begin(9600);
+
+}
+
+void loop() 
+
+{ 
+
+int chk = DHT.read11(3);
+
+Serial.print("Humidity: " );
+
+Serial.println(DHT.humidity, 1);
+
+Serial.print("Temparature: ");
+
+Serial.println(DHT.temperature, 1);
+
+delay(2000);
+
+}
