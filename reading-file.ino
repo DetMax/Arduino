91829be3725301d9f09e4ac22cfa8f282762b42b
shbhmsxn4 <shbhmsxn4@gmail.com>
2017-12-24 17:33:47 +0530

    Add files via upload

---
+++ reading-file.ino
@@ -0,0 +1,28 @@
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  Serial.println("Connected to computer.");
+  Serial.println("Initialising SD card.");
+  if(!SD.begin(10)){
+    Serial.println("SD card initialisation failed.");
+  }
+  Serial.println("SD card initialised.");
+  dataFile = SD.open("data.txt",FILE_READ);
+  if(dataFile){
+    Serial.println("Data file found and opened.");
+    Serial.println("Its contents are :");
+    while(dataFile.available()){
+      Serial.write(dataFile.read());
+    }
+    dataFile.close();
+    Serial.println("Reading finished.");
+  }
+  else {
+    Serial.println("Data file does not exist.");
+  }
+}
+
+void loop() {
+}
---
+++ writing-temp-and-humidity.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include <SD.h>
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  Serial.println("Connected to computer.");
+  Serial.println("Initialising SD card.");
+  if(!SD.begin(10)){
+    Serial.println("SD card initialisation failed.");
+  }
+  Serial.println("SD card initialised.");
+}
+
+void loop() {
+  //place code here for reading data
+  //assuming temperature is stored in variable 'temperature' and humidity is stored in variable 'humidity'
+  dataFile = SD.open("data.txt",FILE_WRITE);
+  if(dataFile){
+    Serial.println("File opened. Writing data.");
+    dataFile.println("Temperature : "+temperature+" Humidity : "+humidity);
+    Serial.println("Data written.");
+    dataFile.close();
+  }
+  else {
+    Serial.println("Error opening data file.");
+  }
+  //insert delay statement here accordingly
+}
