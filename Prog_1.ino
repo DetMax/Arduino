a5b3ed29a00e95f70fe35cc531d9877002ab99d0
DetMax <31571120+DetMax@users.noreply.github.com>
2017-12-25 09:14:12 +0530

    Add files via upload

---
+++ Prog_1.ino
@@ -0,0 +1,52 @@
+#include <SPI.h>
+#include <SD.h>
+#include<dht.h>
+dht DHT;
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  Serial.println("Connected to computer.");
+  Serial.println("Initialising SD card.");
+  if(!SD.begin(10)){
+    Serial.println("SD card initialisation failed.");
+  }
+  Serial.println("SD card initialised.");
+}
+
+void loop() 
+{
+float sValue;
+float sVolt;
+sValue = analogRead(A0);
+sVolt = sValue/1024*5.0;
+Serial.print("sensor voltage = ");
+Serial.print(sVolt);
+Serial.print("V ");
+Serial.println(sValue);
+int chk = DHT.read11(3);
+Serial.print("Humidity: " );
+Serial.println(DHT.humidity, 1);
+Serial.print("Temparature: ");
+Serial.println(DHT.temperature, 1);
+int temp = DHT.temperature;
+int humidity = DHT.humidity;
+File dataFile = SD.open("data.txt",FILE_WRITE);
+  if(dataFile){
+    dataFile.print("Temperature : ");
+    dataFile.print(temp);
+    dataFile.print(" Humidity : ");
+    dataFile.print(humidity);
+    dataFile.print(" Pollution level : ");
+    dataFile.println(sValue);
+    Serial.println("File updated.");
+    dataFile.close();
+  }
+  else {
+    Serial.println("Error opening data file.");
+  }
+  delay(2000);
+}
+
