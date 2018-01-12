a8974b49bc6914cbea31db65d8909ce37ffc52e3
shbhmsxn4 <shbhmsxn4@gmail.com>
2018-01-13 00:15:37 +0530

    Basic communication with ESP

---
+++ espcombasic.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+    Serial.print("We got : ");
+    Serial.print(Serial.readString());
+  }
+  else{
+    Serial.print('.');
+    delay(1000);
+  }
+}
