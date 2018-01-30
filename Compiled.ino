e197c3b0960209b5ae638fc513ce5de07b903ad3
shbhmsxn4 <shbhmsxn4@gmail.com>
2018-01-30 20:12:53 +0530

    Add files via upload

---
+++ Compiled.ino
@@ -0,0 +1,100 @@
+/*
+ * 
+ * Code State:
+ * Creation (26/1, 4:00 pm)
+ * Serial: Initialised
+ * LCD: Initialised
+ * SDModule: Initialised
+ * 
+ */
+
+#include<LiquidCrystal.h>
+#include<SD.h>
+
+LiquidCrystal lcd(7,6,5,4,3,2);   // (rs, en, d4, d5, d6, d7)
+File myFile;
+String espIP;
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  while(!Serial){
+    ;
+  }
+  lcd.clear();
+  lcd.print("Beginning");
+  lcd.setCursor(0,1);
+  lcd.print("Setup");
+  delay(1000);
+  lcd.clear();
+  lcd.print("SD Module:");
+  if(!SD.begin(53)){             // cs pin
+    printErrorCode(10);
+  }
+  lcd.setCursor(0,1);
+  lcd.print("Initialised");
+  delay(1000);
+  lcd.clear();
+  lcd.print("Waiting for");
+  lcd.setCursor(0,1);
+  lcd.print("ESP");
+  delay(1000);
+  Serial3.begin(9600);
+  while(!Serial3){
+    delay(1);
+  }
+  while(!Serial3.available()){
+    delay(1);
+  }
+  char temp = Serial3.read();
+  while(temp != 'R'){
+    temp = Serial3.read();
+  }
+  lcd.clear();
+  lcd.print("ESP Ready");
+  delay(1000);
+  Serial3.print('R');
+  lcd.clear();
+  lcd.print("Waiting for IP");
+  delay(1000);
+  temp = Serial3.read();
+  while(temp != 'S'){
+    temp = Serial3.read();
+  }
+  espIP = Serial3.readStringUntil('\r');
+  lcd.clear();
+  lcd.print("ESP comm est");
+  lcd.setCursor(0,1);
+  lcd.print(espIP);
+  delay(1000);
+}
+
+void loop() {
+  
+  cleardisp();
+  lcd.print("Project idle");
+  delay(2000);
+  for(int i=0;i<3;i++){
+    lcd.print(".");
+    delay(2000);
+  }
+}
+
+void printErrorCode(int errorCode){
+  while(1>0){
+    lcd.clear();
+    lcd.print("Error ");
+    lcd.print(errorCode);
+    for(int i=0;i<3;i++){
+      lcd.print(".");
+      delay(1000);
+    }
+  }
+}
+
+void cleardisp(){
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print(espIP);
+  lcd.setCursor(0,0);
+}
+
---
+++ ESPBasic.ino
@@ -0,0 +1,51 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "Gasolina";       //WiFi SSID
+const char* password = "DameMaGasolina";    //WiFi Password
+
+WiFiServer server(80);                //service port
+
+void setup() {
+  delay(10000);
+  Serial.begin(9600);
+  while(!Serial){
+    delay(1);
+  }
+  Serial.setTimeout(5000);
+  delay(10);
+  Serial.print('R');
+  while(!Serial.available()){
+    delay(1);
+  }
+  char temp = Serial.read();
+  while(temp != 'R'){
+    temp = Serial.read();
+  }
+  WiFi.begin(ssid, password);
+  while(WiFi.status()!=WL_CONNECTED){
+    delay(500);
+  }
+  Serial.print('S');
+  server.begin();
+  Serial.print(WiFi.localIP());
+  Serial.print('\r');
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if(!client){
+    return;
+  }
+  while(!client.available()){       //wait until client sends some data
+    delay(1);
+  }
+  char request = client.read();
+  Serial.println(request);
+  while(!Serial.available()){      //wait until arduino sends back response
+    delay(1);
+  }
+  String response = Serial.readStringUntil('\r');
+  client.println(response);
+  client.flush();
+  delay(1);
+}
